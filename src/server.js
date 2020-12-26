86ec95a64b42ad28f8f4080afc4a50b1f23e5e97
Ed <lookatemail@gmail.com>
2020-12-26 20:49:41 +0000

    Add router object to server.js

--- src/server.js
+++ src/server.js
@@ -2,13 +2,18 @@ const express = require('express');
 const serverless = require('serverless-http');
 
 const app = express();
+const router = express.Router();
 
-app.get('/', (req, res) => {
-  res.send('GET request to the homepage');
+// Middleware
+app.use('/.netlify/functions/server', router);
+
+router.get('/', (req, res) => {
+  res.send('GET request to the Serverless App homepage');
   // res.json({ data: 'Hello World' });
 });
 
 // In order to alow lambda to run exporting handler function
+module.exports = app;
 module.exports.handler = serverless(app);
 
 // Express app port No
