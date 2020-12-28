7c60582823339e2f42879115a765983eb247bc44
Ed <lookatemail@gmail.com>
2020-12-28 16:26:04 +0000

    Add static folder as middleware

--- src/server.js
+++ src/server.js
@@ -1,18 +1,25 @@
 const express = require('express');
 const serverless = require('serverless-http');
+const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 const router = express.Router();
 
 // Middleware
-app.use('/.netlify/functions/server', router);
-
-// View engine via ejs - able to render static file
-app.set('views', __dirname + '/../dist');
-app.set('view engine', 'html');
-app.engine('html', require('ejs').renderFile);
+app.use(express.static(path.join(__dirname, '../dist'))); // Set static folder
+app.use('/.netlify/functions/server', router); // Set route end point
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+); // Body parser
+app.use(bodyParser.json());
 
 // Routes
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../dist/index.html'));
+// });
 router.get('/', (req, res) => {
   res.send('GET request to the Serverless App homepage');
 });
@@ -20,11 +27,21 @@ router.get('/json', (req, res) => {
   res.json({ data: 'Hello World' });
 });
 router.get('/index', (req, res) => {
-  // res.send('GET request to the index page');
+  res.render('index.html', { form: 'login form' });
+});
+router.post('/submit', (req, res) => {
+  if (req.body) {
+    const { username, password } = req.body;
+    console.log(username, password);
+  } else {
+    console.log('error');
+  }
+  // console.log('submiting...', { username: username, password: password });
+  // res.send('login');
   res.render('index.html', { form: 'login form' });
 });
 
-// In order to alow lambda to run exporting handler function
+// In order to alow lambda to run - exporting handler function
 module.exports = app;
 module.exports.handler = serverless(app);
 
