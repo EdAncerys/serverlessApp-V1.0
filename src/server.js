b451bf6fda2f25f182364d06c68b8ca55d13f940
Ed <lookatemail@gmail.com>
2020-12-28 21:17:04 +0000

    Istall moment/date package

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const serverless = require('serverless-http');
 const path = require('path');
+
 const bodyParser = require('body-parser');
 
 const app = express();
 const router = express.Router();
+const logger = require('./logger');
 
-// Middleware
+// Init Middleware
 app.use(express.static(path.join(__dirname, '../dist'))); // Set static folder
 app.use('/.netlify/functions/server', router); // Set route end point
 app.use(
@@ -15,31 +17,32 @@ app.use(
   })
 ); // Body parser
 app.use(bodyParser.json());
+app.use(logger);
 
 // Routes
-app.get('/c', (req, res) => {
-  res.sendFile(path.join(__dirname, '../dist', 'contact.html'));
+app.get('/route', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist', 'index.html'));
 });
-router.get('/', (req, res) => {
-  res.send('GET request to the Serverless App homepage');
-});
-router.get('/json', (req, res) => {
-  res.json({ data: 'Hello World' });
-});
-router.get('/index', (req, res) => {
-  res.render('index.html', { form: 'login form' });
-});
-router.post('/submit', (req, res) => {
-  if (req.body) {
-    const { username, password } = req.body;
-    console.log(username, password);
-  } else {
-    console.log('error');
-  }
-  // console.log('submiting...', { username: username, password: password });
-  // res.send('login');
-  res.render('index.html', { form: 'login form' });
-});
+// router.get('/', (req, res) => {
+//   res.send('GET request to the Serverless App homepage');
+// });
+// router.get('/json', (req, res) => {
+//   res.json({ data: 'Hello World' });
+// });
+// router.get('/index', (req, res) => {
+//   res.render('index.html', { form: 'login form' });
+// });
+// router.post('/submit', (req, res) => {
+//   if (req.body) {
+//     const { username, password } = req.body;
+//     console.log(username, password);
+//   } else {
+//     console.log('error');
+//   }
+//   // console.log('submiting...', { username: username, password: password });
+//   // res.send('login');
+//   res.render('index.html', { form: 'login form' });
+// });
 
 // In order to alow lambda to run - exporting handler function
 module.exports = app;
