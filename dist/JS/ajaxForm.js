9d871e47d205e75384c523cd3db4f38c11459320
Ed <lookatemail@gmail.com>
2021-01-03 10:02:32 +0000

    Add file tree to views folder

--- dist/JS/ajaxForm.js
+++ dist/JS/ajaxForm.js
@@ -11,11 +11,16 @@ $(function () {
       .done(function (res) {
         // code if form was successfully sent
         const data = res;
+        const formID = form[0].id;
         document.querySelector('#data').innerHTML = data;
 
-        $('#formElement').trigger('reset').hide(); //reset form
+        if (formID === 'lambdaForm') $('#lambdaForm').trigger('reset').hide(); //reset form
+        if (formID === 'freshDeskForm')
+          $('#freshDeskForm').trigger('reset').hide(); //reset form
         $('.msg').show();
+
         console.log(`Form Submitted Successfully`);
+        console.log(form[0].id);
         console.log(data, lambda);
       })
       .fail(function (error) {
