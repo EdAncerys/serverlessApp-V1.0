a7f55ae3c672d5ecf76d741ac2c5f3e586df0829
Ed <lookatemail@gmail.com>
2021-01-09 12:51:58 +0000

    Refactor index.html

--- js/main.js
+++ js/main.js
@@ -6,43 +6,24 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function freshDeskTickets(ev) {
   ev.preventDefault();
-  const URL = '/ndg/freshDeskTickets';
+  const URL = '/ndg/tickets';
   console.log('Fetching freshDeskTickets...');
   fetch(URL)
     .then((resp) => resp.json())
-    .then((content) => {
+    .then((data) => {
       let main = document.querySelector('main');
-      main.innerHTML = `<h2>${content}</h2>`;
+      let content = data.map((ticket) => {
+        return `<li>ID: ${ticket.requester_id}</li>
+                <li>Created At: ${ticket.created_at}</li>
+                <li>Subject: ${ticket.subject}</li>
+                <li>Type: ${ticket.type}</li>`;
+      });
+      main.innerHTML = `<ul>${content}</ul>`;
       console.log('Done...');
     })
     .catch((err) => {
-      console.log(JSON.stringify(err));
+      let main = document.querySelector('main');
+      main.innerHTML = `<h4>${err}</h4>`;
+      console.log(err);
     });
-}
-
-function getYes(ev) {
-  console.log('yes');
-  let url = '/api/yes';
-  fetch(url)
-    .then((resp) => resp.json())
-    .then((content) => {
-      let main = document.querySelector('main');
-      main.innerHTML = `<h2>${content.msg}</h2>`;
-    })
-    .catch((err) => console.error);
-}
-
-function getOcto(ev) {
-  ev.preventDefault();
-  console.log('get octocat');
-  fetch('/api/octo')
-    .then((res) => res.json())
-    .then((data) => {
-      let main = document.querySelector('main');
-      let content = data.map((item) => {
-        return `<li>${item.name}</li>`;
-      });
-      main.innerHTML = `<ul>${content}</ul>`;
-    })
-    .catch(console.error);
 }
