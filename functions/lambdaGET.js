4ca47f4afa659449cdc9457d9773a4861cd07355
Ed <lookatemail@gmail.com>
2021-01-10 11:32:40 +0000

    Refactor file structure V.1.0

---
+++ functions/lambdaGET.js
@@ -0,0 +1,11 @@
+exports.handler = function (event, context, callback) {
+  const body = {
+    data: 'Hello World!',
+    responseCode: 200,
+  };
+
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify(body),
+  });
+};
