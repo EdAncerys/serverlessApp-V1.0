9f412c30694341f808c5f2f321b60cb5ef1fce5d
Ed <lookatemail@gmail.com>
2021-01-21 23:13:18 +0000

    Remove ability to change background if header selected

--- dist/js/_helperFunctions/_icukBroadband/_handleBroadbandSelection.js
+++ dist/js/_helperFunctions/_icukBroadband/_handleBroadbandSelection.js
@@ -1,8 +1,12 @@
 const _handleBroadbandSelection = (event) => {
-  const closestCell = event.target.closest('tr'),
-    activeCell = event.currentTarget.querySelector('tr.selected');
-  closestCell.classList.add('selected');
-  if (activeCell) activeCell.classList.remove('selected');
+  const header = event.target.parentNode.children[1].innerHTML;
+
+  if (header !== 'Supplier') {
+    const closestCell = event.target.closest('tr'),
+      activeCell = event.currentTarget.querySelector('tr.selected');
+    closestCell.classList.add('selected');
+    if (activeCell) activeCell.classList.remove('selected');
+  }
 };
 
 export { _handleBroadbandSelection };
