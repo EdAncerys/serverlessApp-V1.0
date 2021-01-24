e039527e46d57e0b467cb1c2be74d4fc2a32c92e
Ed <lookatemail@gmail.com>
2021-01-24 14:56:03 +0000

    Refactor nodeMailer - add env and refactor passing objects

--- functions/nodeMailerIONOS.js
+++ functions/nodeMailerIONOS.js
@@ -2,35 +2,30 @@ const nodemailer = require('nodemailer');
 require('dotenv').config(); // Enabling to load Environment variables from a .env File
 
 exports.handler = function (event, context, callback) {
-  // const body = JSON.parse(event.body);
-  const subject = 'Nodemailer is unicode friendly ✔';
-  const emailBody = '<p><b>Hello</b> to myself!</p>';
-
+  console.log(event);
+  const emailBody = JSON.parse(event.body);
+  const name = emailBody.name;
+  const email = emailBody.email;
+  const subject =
+    emailBody.subject + ' | Created at: ' + new Date().toLocaleString() + ' |';
+  const description = emailBody.description;
   console.log('Credentials obtained, sending message...');
 
-  // const transporter = nodemailer.createTransport({
-  //   service: process.env.EMAIL_SERVICE, // replace with service provider
-  //   auth: {
-  //     user: process.env.EMAIL_NAME, // replace with your email
-  //     pass: process.env.EMAIL_PASSWORD, // replace with your password
-  //   },
-  // });
-
   const transporter = nodemailer.createTransport({
-    host: 'smtp.ionos.co.uk',
-    port: 465,
-    secure: true,
+    host: process.env.IONOS_HOST,
+    port: process.env.IONOS_PORT,
+    secure: process.env.IONOS_SECURE,
     auth: {
-      user: 'ed.ancerys@ndg-technology.com', // replace with your email
-      pass: 'NDGT3chn0l0gy1234!@', // replace with your password
+      user: process.env.IONOS_USER, // replace with your email
+      pass: process.env.IONOS_PASS, // replace with your password
     },
   });
 
   const mailOptions = {
-    from: 'Email from IONOS account', // replace with your email
-    to: 'ed.ancerys@ndg-technology.com', // replace with your mailing list
+    from: `"Fred Foo 👻" <${process.env.IONOS_USER}>`, // replace with your email
+    to: process.env.IONOS_MAILING_LIST, // replace with your mailing list
     subject: `${subject}`,
-    html: `${emailBody}`,
+    html: `${description}`,
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
