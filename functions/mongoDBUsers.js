5a6fdf2486dc6af52890d2d03de967241a6771db
Ed <lookatemail@gmail.com>
2021-01-29 07:25:48 +0000

    Able to conect to mongoDB and fetch data

---
+++ functions/mongoDBUsers.js
@@ -0,0 +1,43 @@
+const MongoClient = require('mongodb').MongoClient;
+
+const MONGODB_URI = process.env.MONGODB_URI;
+const DB_NAME = 'ndgUsers';
+const COLLECTION = 'ndg_users';
+
+let cachedDb = null;
+
+const connectToDatabase = async (uri) => {
+  // we can cache the access to our database to speed things up a bit
+  // (this is the only thing that is safe to cache here)
+  if (cachedDb) return cachedDb;
+
+  const client = await MongoClient.connect(uri, {
+    useUnifiedTopology: true,
+  });
+
+  cachedDb = client.db(DB_NAME);
+
+  return cachedDb;
+};
+
+const queryDatabase = async (db) => {
+  const dbData = await db.collection(COLLECTION).find({}).toArray();
+  console.table(dbData);
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(dbData),
+  };
+};
+
+module.exports.handler = async (event, context) => {
+  // otherwise the connection will never complete, since
+  // we keep the DB connection alive
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  const db = await connectToDatabase(MONGODB_URI);
+  return queryDatabase(db);
+};
