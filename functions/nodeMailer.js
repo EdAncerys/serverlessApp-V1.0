e96550baf9cad3651dc1e32751f061ae4ff227e3
Ed <lookatemail@gmail.com>
2021-01-30 17:14:22 +0000

    Ectract emailTemplate to a helper

--- functions/nodeMailer.js
+++ functions/nodeMailer.js
@@ -9,83 +9,7 @@ exports.handler = (event, context, callback) => {
   let subject = body.subject;
   let description = body.description;
 
-  const tableCellStyle = `style="border: 1px solid #c1c1c1;
-                          color: #2b2b2b;
-                          font-size: 16px;
-                          font-weight: normal;
-                          padding: 20px;
-                          text-shadow: 0 1px 1px rgba(256, 256, 256, 0.1);"`;
-  const emailTemplate = `<div style="display: grid; justify-content: center">
-                        <table style="background-color: #f4f4f4; min-width: 400px; margin: 20px">
-                          <tr>
-                            <th
-                            colspan="2"
-                            style="
-                              color: #f4f4f4;
-                              background: #2b2b2b;
-                              border: 1px solid #343a45;
-                              text-align: center;
-                              text-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);
-                              vertical-align: middle;
-                              padding: 10px;
-                            "
-                            >
-                              <div style="display: grid; justify-content: center">
-                                <img src="cid:ndgLogo" alt="ndgLogo"/>
-                              </div>
-                            </th>
-                          </tr>
-
-                          <tr style="padding: 5px">
-                            <th
-                            ${tableCellStyle}
-                            >
-                              Name
-                            </th>
-                            <th
-                              ${tableCellStyle}
-                            >
-                              ${name}
-                            </th>
-                          </tr>
-                          <tr style="padding: 5px">
-                            <th
-                              ${tableCellStyle}
-                            >
-                              Email
-                            </th>
-                            <th
-                              ${tableCellStyle}
-                            >
-                              ${email}
-                            </th>
-                          </tr>
-                          <tr style="padding: 5px">
-                            <th
-                              ${tableCellStyle}
-                            >
-                              Subject
-                            </th>
-                            <th
-                              ${tableCellStyle}
-                            >
-                              ${subject}
-                            </th>
-                          </tr>
-                          <tr style="padding: 5px">
-                            <th
-                              ${tableCellStyle}
-                            >
-                              Description
-                            </th>
-                            <th
-                              ${tableCellStyle}
-                            >
-                              ${description}
-                            </th>
-                          </tr>
-                        </table>
-                        </div>`;
+  const emailTemplate = emailTemplateForm(name, email, subject, description);
 
   const transporter = nodemailer.createTransport({
     service: process.env.EMAIL_SERVICE, // replace with service provider
@@ -124,4 +48,84 @@ exports.handler = (event, context, callback) => {
       console.log('Email sent successfully');
     }
   });
+};
+
+const emailTemplateForm = (name, email, subject, description) => {
+  const tableCellStyle = `style="border: 1px solid #c1c1c1;
+                          color: #2b2b2b;
+                          font-size: 16px;
+                          font-weight: normal;
+                          padding: 20px;
+                          text-shadow: 0 1px 1px rgba(256, 256, 256, 0.1);"`;
+  return `<div style="display: grid; justify-content: center">
+            <table style="background-color: #f4f4f4; min-width: 400px; margin: 20px">
+              <tr>
+                <th
+                colspan="2"
+                style="
+                  color: #f4f4f4;
+                  background: #2b2b2b;
+                  border: 1px solid #343a45;
+                  text-align: center;
+                  text-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);
+                  vertical-align: middle;
+                  padding: 10px;
+                "
+                >
+                  <div style="display: grid; justify-content: center">
+                    <img src="cid:ndgLogo" alt="ndgLogo"/>
+                  </div>
+                </th>
+              </tr>
+
+              <tr style="padding: 5px">
+                <th
+                ${tableCellStyle}
+                >
+                  Name
+                </th>
+                <th
+                  ${tableCellStyle}
+                >
+                  ${name}
+                </th>
+              </tr>
+              <tr style="padding: 5px">
+                <th
+                  ${tableCellStyle}
+                >
+                  Email
+                </th>
+                <th
+                  ${tableCellStyle}
+                >
+                  ${email}
+                </th>
+              </tr>
+              <tr style="padding: 5px">
+                <th
+                  ${tableCellStyle}
+                >
+                  Subject
+                </th>
+                <th
+                  ${tableCellStyle}
+                >
+                  ${subject}
+                </th>
+              </tr>
+              <tr style="padding: 5px">
+                <th
+                  ${tableCellStyle}
+                >
+                  Description
+                </th>
+                <th
+                  ${tableCellStyle}
+                >
+                  ${description}
+                </th>
+              </tr>
+            </table>
+            </div>`;
 };
