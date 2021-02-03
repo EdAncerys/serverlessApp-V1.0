5274215c1bd12d6cf361804f7c6457e1b31d496f
Ed <lookatemail@gmail.com>
2021-02-03 08:08:11 +0000

    Add user login lambda

---
+++ functions/mongoDB_oneTouchLogin.js
@@ -0,0 +1,71 @@
+const MongoClient = require('mongodb').MongoClient;
+
+const MONGODB_URI = process.env.MONGODB_URI;
+const DB_NAME = 'oneTouchDB';
+const COLLECTION = 'oneTouchUsers';
+
+let cachedDb = null;
+
+const connectToDatabase = async (uri) => {
+  if (cachedDb) return cachedDb;
+
+  const client = await MongoClient.connect(uri, {
+    useUnifiedTopology: true,
+  });
+
+  cachedDb = client.db(DB_NAME);
+
+  return cachedDb;
+};
+
+const oneTouchLogin = async (db, data) => {
+  const loginUser = {
+    email: data.email,
+    password: data.password,
+  };
+  const user = await db
+    .collection(COLLECTION)
+    .find({ email: loginUser.email })
+    .toArray();
+  const userValid = !user[0];
+
+  if (userValid && loginUser.password) {
+    const msg =
+      `Successful logged in! Welcome to One Touch Portal ` + loginUser.email;
+    console.log(msg);
+
+    return {
+      statusCode: 201,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ user: data, msg: msg }),
+    };
+  } else {
+    const msg =
+      `Access denied! Username or password do not exist or mach for: ` +
+      loginUser.email;
+    console.log(msg);
+
+    return {
+      statusCode: 422,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ user: data, msg: msg }),
+    };
+  }
+};
+
+module.exports.handler = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  const db = await connectToDatabase(MONGODB_URI);
+
+  switch (event.httpMethod) {
+    case 'GET':
+      return oneTouchLogin(db);
+    default:
+      return { statusCode: 400 };
+  }
+};
