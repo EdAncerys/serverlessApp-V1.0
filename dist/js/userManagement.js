1562ffb03b56a3bd10164551e25f56ff3efbd8c9
Ed <lookatemail@gmail.com>
2021-02-21 15:36:18 +0000

    Able to save DOM to sessionStorage on change

--- dist/js/userManagement.js
+++ dist/js/userManagement.js
@@ -8,6 +8,36 @@ document.addEventListener('DOMContentLoaded', () => {
     .addEventListener('click', createOneTouchAccount);
 });
 
+if (performance.navigation.type == PerformanceNavigation.TYPE_RELOAD) {
+  console.info('This page is reloaded');
+  const oneTouchUsers = document.querySelector('oneTouchUsers');
+  const domBody = sessionStorage.getItem('body');
+  // document.body.innerHTML = domBody;
+  // oneTouchUsers.innerHTML = domBody;
+  // console.log(domBody);
+}
+
+const observer = new MutationObserver((list) => {
+  const evt = new CustomEvent('dom-changed', { detail: list });
+  document.body.dispatchEvent(evt);
+});
+observer.observe(document.body, {
+  attributes: true,
+  childList: true,
+  subtree: true,
+});
+
+document.body.addEventListener('dom-changed', (e) => {
+  console.info('Saving Data to sessionStorage...');
+  const body = document.body.innerHTML;
+  sessionStorage.setItem('body', body);
+});
+
+const persistUserData = () => {
+  const body = document.querySelector('body');
+  console.log(body);
+};
+
 const getAllUsers = (ev) => {
   ev.preventDefault();
   _oneTouchUsers();
