d46da11b244f0584860977395df2ca50b21dce02
Ed <lookatemail@gmail.com>
2021-02-23 12:46:34 +0000

    Able to persist user DOM data in UI

--- dist/js/orderManagement.js
+++ dist/js/orderManagement.js
@@ -6,6 +6,31 @@ document.addEventListener('DOMContentLoaded', () => {
     .addEventListener('click', oneTouchOrders);
 });
 
+// Persist user data on reload
+if (performance.navigation.type == PerformanceNavigation.TYPE_RELOAD) {
+  console.info('This page is reloaded');
+  const oneTouchDOMBody = sessionStorage.getItem('oneTouchDOMBody');
+  const body = document.querySelector('body');
+  body.innerHTML = oneTouchDOMBody;
+}
+// Create custom event
+const observer = new MutationObserver((list) => {
+  const evt = new CustomEvent('dom-changed', { detail: list });
+  document.body.dispatchEvent(evt);
+});
+// Listen to DOM changes
+observer.observe(document.body, {
+  attributes: true,
+  childList: true,
+  subtree: true,
+});
+// Save DOM changes to localStorage
+document.body.addEventListener('dom-changed', (e) => {
+  console.info('Saving Data to sessionStorage...');
+  const oneTouchDOMBody = document.body.innerHTML;
+  sessionStorage.setItem('oneTouchDOMBody', oneTouchDOMBody);
+});
+
 const oneTouchOrders = (ev) => {
   ev.preventDefault();
   _oneTouchOrders();
