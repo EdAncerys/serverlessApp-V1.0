f8c5ab6926259130c4b309ac76c8a8d1fd399959
Ed <lookatemail@gmail.com>
2021-02-24 22:16:46 +0000

    Placing error messages in absolute position - fix styling

--- dist/js/_helperFunctions/_errorMessage.js
+++ dist/js/_helperFunctions/_errorMessage.js
@@ -21,50 +21,50 @@ const _errorMessage = (message, type) => {
     msgColor = '#d9534f';
   }
 
-  const errorMsg = `  <div
+  const errorMsg = `<div
+                      style="
+                        display: grid;
+                        justify-items: center;
+                      "
+                    >
+                      <div
                         style="
                           display: grid;
-                          justify-items: center;
+                          grid-template-columns: 10px 1fr 6fr 40px;
+                          width: 80%;
+                          color: #f7f7f7;
+                          margin: 5px 0;
+                          border-radius: 10px;
+                          background-color: ${msgColor};
+                          opacity: 0.8;
                         "
                       >
-                        <div
-                          style="
-                            display: grid;
-                            grid-template-columns: 10px 1fr 6fr 40px;
-                            width: 80%;
-                            color: #f7f7f7;
-                            margin: 10px 0;
-                            border-radius: 10px;
-                            background-color: ${msgColor};
-                            opacity: 0.8;
-                          "
-                        >
-                          <div style="border-radius: 10px 0 0 10px; height: 60px; background-color: #dfdfdf"></div>
-                          <div style="
-                                display: grid;
-                                align-content: center;
-                                padding-left: 10px;
-                                ">
-                                <strong>${msgType}</strong>
-                          </div>
-                          <div style="
-                          display: grid;
-                          align-content: center;
-                          overflow: auto;
-                          ">
-                            ${message}
-                          </div>
-                          <closeErrorMsg id='${msgID}'
-                                style="
-                                cursor: pointer;
-                                display: grid;
-                                justify-items: center;
-                                align-content: center;
-                                "
-                                > &times; 
-                          </closeErrorMsg>
+                        <div style="border-radius: 10px 0 0 10px; height: 60px; background-color: #dfdfdf"></div>
+                        <div style="
+                              display: grid;
+                              align-content: center;
+                              padding-left: 10px;
+                              ">
+                              <strong>${msgType}</strong>
                         </div>
-                      </div>`;
+                        <div style="
+                        display: grid;
+                        align-content: center;
+                        overflow: auto;
+                        ">
+                          ${message}
+                        </div>
+                        <closeErrorMsg id='${msgID}'
+                              style="
+                              cursor: pointer;
+                              display: grid;
+                              justify-items: center;
+                              align-content: center;
+                              "
+                              > &times; 
+                        </closeErrorMsg>
+                      </div>
+                    </div>`;
 
   let newErrorMsg = document.createElement('div');
   newErrorMsg.id = `errorMessage_${msgID}`;
