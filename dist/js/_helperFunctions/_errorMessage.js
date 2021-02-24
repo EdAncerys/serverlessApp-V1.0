83e258568566dbfea06d2279a705a02d96464d02
Ed <lookatemail@gmail.com>
2021-02-24 21:23:23 +0000

    Able to dismiss individual error msg

--- dist/js/_helperFunctions/_errorMessage.js
+++ dist/js/_helperFunctions/_errorMessage.js
@@ -1,14 +1,15 @@
 const _errorMessage = (message, type) => {
-  let errorMessage = document.querySelector('errorMessage');
+  let errorMessageContainer = document.querySelector('errorMessage');
   let msgID = Date.now().toString();
-  errorMessage.style.visibility = 'visible';
-  const closeErrorMsg = () => {
-    errorMessage.style.visibility = 'hidden';
+  // errorMessage.style.visibility = 'visible';
+  const closeErrorMsg = (id) => {
+    console.log(id);
+    // errorMessage.remove();
   };
 
   setTimeout(() => {
-    let errorMessage = document.getElementById(`errorMessage_${msgID}`);
-    if (errorMessage) errorMessage.style.visibility = 'hidden';
+    let errMsg = document.getElementById(`errorMessage_${msgID}`);
+    if (errMsg) errorMessage.remove();
   }, 4000);
 
   let msgType;
@@ -25,37 +26,68 @@ const _errorMessage = (message, type) => {
     msgColor = '#d9534f';
   }
 
-  const errorMsg = `<div
-                    id='errorMessage_${msgID}'
-                    style="
-                      display: grid;
-                      position: absolute;
-                      grid-template-columns: 1% 15% 75% 4%;
-                      align-items: center;
-                      width: 80vw;
-                      height: 60px;
-                      color: #f7f7f7;
-                      margin: 2vw 0 0 10vw;
-                      border-radius: 10px;
-                      background-color: ${msgColor};
-                      opacity: 0.8;
-                    "
-                  >
-                    <div style="border-radius: 10px 0 0 10px; height: 100%; background-color: #292b2c"></div>
-                    <div style="padding-left: 10%"><strong>${msgType}</strong></div>
-                    <div>${message}</div>
-                    <div id='${msgID}'
-                          style="cursor: pointer; padding-left: 50%;"> &times; </div>
-                  </div>`;
-
-  setTimeout(() => {
-    let errorMessage = document.getElementById(`${msgID}`);
-    if (errorMessage) errorMessage.onclick = closeErrorMsg;
-  }, 100);
+  const errorMsg = `  <div
+                        style="
+                          display: grid;
+                          justify-items: center;
+                        "
+                      >
+                        <div
+                          style="
+                            display: grid;
+                            grid-template-columns: 10px 1fr 6fr 40px;
+                            width: 80%;
+                            color: #f7f7f7;
+                            margin: 10px 0;
+                            border-radius: 10px;
+                            background-color: ${msgColor};
+                            opacity: 0.8;
+                          "
+                        >
+                          <div style="border-radius: 10px 0 0 10px; height: 60px; background-color: #dfdfdf"></div>
+                          <div style="
+                                display: grid;
+                                align-content: center;
+                                padding-left: 10px;
+                                ">
+                                <strong>${msgType}</strong>
+                          </div>
+                          <div style="
+                          display: grid;
+                          align-content: center;
+                          overflow: auto;
+                          ">
+                            ${message}
+                          </div>
+                          <closeErrorMsg id='${msgID}'
+                                style="
+                                cursor: pointer;
+                                display: grid;
+                                justify-items: center;
+                                align-content: center;
+                                "
+                                > &times; 
+                          </closeErrorMsg>
+                        </div>
+                      </div>`;
 
   let newErrorMsg = document.createElement('div');
+  newErrorMsg.id = `errorMessage_${msgID}`;
   newErrorMsg.innerHTML = errorMsg;
-  errorMessage.appendChild(newErrorMsg);
+  errorMessageContainer.appendChild(newErrorMsg);
+
+  document
+    .getElementById(`errorMessage_${msgID}`)
+    .addEventListener('click', (event) => {
+      const closeErrorMsg = event.target.nodeName === 'CLOSEERRORMSG';
+
+      let id = event.target.getAttribute('id');
+
+      if (closeErrorMsg) {
+        let errMsg = document.getElementById(`errorMessage_${msgID}`);
+        if (errMsg) errMsg.remove();
+      }
+    });
 };
 
 export { _errorMessage };
