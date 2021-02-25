3ab3ad59801ef1842f1bcd7f469aa3316f6f2ab6
Ed <lookatemail@gmail.com>
2021-02-25 13:32:00 +0000

    Refactor spinner helper function

--- dist/js/_helperFunctions/_spinner.js
+++ dist/js/_helperFunctions/_spinner.js
@@ -1,35 +1,24 @@
-// const _spinner = (value) => {
-//   const spinner = document.querySelector('#spinner');
-//   if (value) {
-//     console.log('Spinner on');
-//     // spinner.style.display = 'block';
-//   } else {
-//     console.log('Spinner off');
-//     // spinner.style.display = 'none';
-//   }
-// };
-
-// export { _spinner };
-
 const _spinner = (active, msg) => {
   let spinnerContainer = document.querySelector('spinnerContainer');
-  let spinnerIcon = document.createElement('spinnerIcon');
-  let spinnerIconContainer = document.createElement('spinnerContainer');
+
+  let spinnerWrapper = document.createElement('spinnerWrapper');
+  let spinnerIconContainer = document.createElement('spinnerIconContainer');
+  let spinner = document.createElement('spinner');
   let spinnerMsg = document.createElement('spinnerMsg');
   const newMsg = msg ? msg : 'Loading Selection...';
-  console.log('Spinner function', newMsg);
 
   if (active) {
-    spinnerContainer.appendChild(spinner);
-    spinnerContainer.appendChild(spinnerMsg);
+    spinnerContainer.appendChild(spinnerWrapper);
+    spinnerWrapper.appendChild(spinnerIconContainer);
+    spinnerIconContainer.appendChild(spinner);
+    spinnerWrapper.appendChild(spinnerMsg);
+    spinnerWrapper.classList.add('spinnerWrapper');
     spinner.classList.add('fa', 'fa-cog', 'fa-spin');
     spinnerMsg.innerHTML = newMsg;
   }
   if (!active) {
-    let spinnerDel = document.querySelector('spinner');
-    let spinnerMsgDel = document.querySelector('spinnerMsg');
+    let spinnerDel = document.querySelector('spinnerWrapper');
     if (spinnerDel) spinnerDel.remove();
-    if (spinnerMsgDel) spinnerMsgDel.remove();
   }
 };
 
