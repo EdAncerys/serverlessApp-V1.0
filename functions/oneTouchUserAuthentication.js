778c8c866a8fe99173359589588e24308f50b245
Ed <lookatemail@gmail.com>
2021-03-13 11:43:54 +0000

    Add login btn for easy access of login page

--- functions/oneTouchUserAuthentication.js
+++ functions/oneTouchUserAuthentication.js
@@ -1,6 +1,6 @@
 exports.handler = async (event, context, callback) => {
   // const redirectURL = './oneTouch/one-touch-login.html';
-  const redirectURL = '/views/oneTouch/login.html';
+  const redirectURL = '/views/oneTouch/ne-touch-login.html';
   console.log('Access denied. You been redirected to: ' + redirectURL);
 
   return {
