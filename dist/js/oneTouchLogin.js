0d2519b29b6b86218b7ea18564c207b201965669
Ed <lookatemail@gmail.com>
2021-03-30 14:35:04 +0100

    Refactor oneTouchLogin helper

--- dist/js/oneTouchLogin.js
+++ dist/js/oneTouchLogin.js
@@ -2,39 +2,17 @@ import { _oneTouchSupperUserLogin } from './helperFunctions/mongoDB/oneTouchLogi
 import { _errorMessage } from './helperFunctions/_errorMessage.js';
 import { _oneTouchUserAuthentication } from './helperFunctions/mongoDB/oneTouchLogin/_oneTouchUserAuthentication.js';
 
-document.addEventListener('DOMContentLoaded', () => {
-  document
-    .querySelector('#oneTouchLogin')
-    .addEventListener('click', oneTouchLogin);
-
-  document
-    .querySelector('authenticateUser')
-    .addEventListener('click', authenticateUser);
-});
-
-const oneTouchLogin = (e) => {
-  e.preventDefault();
-  _oneTouchSupperUserLogin();
-};
-
 document.querySelector('body').addEventListener('click', (event) => {
+  const oneTouchLogin = event.target.id === 'oneTouchLogin';
   const oneTouchSignUp = event.target.id === 'oneTouchSignUp';
 
   // console.log(event.target);
+  if (oneTouchLogin) {
+    _oneTouchSupperUserLogin();
+    return;
+  }
   if (oneTouchSignUp) {
     window.location.replace('/views/oneTouch/one-touch-signup.html');
     return;
   }
 });
-
-async function authenticateUser(e) {
-  e.preventDefault();
-  const authenticated = await _oneTouchUserAuthentication();
-  console.log(authenticated);
-  if (authenticated) {
-    console.log('Authorized');
-  } else {
-    console.log('Unauthorized access');
-    // window.location.replace('/views/oneTouch/one-touch-login.html');
-  }
-}
