3e0f80b11b0e6901cbaf843f0b136beb92ece985
Ed <lookatemail@gmail.com>
2021-03-31 16:16:43 +0100

    Change signup redirect url

--- dist/js/helperFunctions/mongoDB/oneTouchLogin/_oneTouchSupperUserSignup.js
+++ dist/js/helperFunctions/mongoDB/oneTouchLogin/_oneTouchSupperUserSignup.js
@@ -60,7 +60,7 @@ async function _oneTouchSupperUserSignup() {
 
     console.log(data);
     _spinner(false);
-    window.location.replace('/views/oneTouch/index.html');
+    window.location.replace('/views/oneTouch/one-touch-login.html');
   } catch (err) {
     console.log(err);
     _errorMessage(err);
