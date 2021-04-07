6172ea210ef6021010fd1bc08b5263142eb82464
Ed <lookatemail@gmail.com>
2021-04-07 14:12:38 +0100

    Add persist dom data functionality to nav bar - able to log any page on visit

--- dist/views/oneTouch/JS/authenticateUserNavBar.js
+++ dist/views/oneTouch/JS/authenticateUserNavBar.js
@@ -3,22 +3,5 @@ import { persistDOMData } from '../../../js/persistDOMData.js';
 
 _oneTouchUserAuthentication(); // User authentication
 
-// Persist user data on reload
-// const endPoint = location.href.split('/').slice(-1)[0];
-// console.log(endPoint);
-// console.log(sessionStorage.getItem('oneTouchPageName'));
-// sessionStorage.setItem('oneTouchPageName', endPoint);
-// const oneTouchDOMBody = sessionStorage.getItem('oneTouchDOMBody') === null;
-// const oneTouchPageName =
-//   sessionStorage.getItem('oneTouchPageName') === endPoint;
-// console.log(oneTouchPageName);
-
-// if (!oneTouchDOMBody && oneTouchPageName) {
-//   console.log('Page Reload');
-//   const oneTouchDOMBody = document.querySelector('#oneTouchBodyContainer');
-//   oneTouchDOMBody.innerHTML = sessionStorage.getItem('oneTouchDOMBody');
-// }
-// if (oneTouchDOMBody && !oneTouchPageName) {
-//   console.log('Save DOM data');
-//   persistDOMData('oneTouchBodyContainer', endPoint);
-// }
+const endPoint = location.href.split('/').slice(-1)[0];
+persistDOMData(endPoint);
