192efdfd87b89cf2504f3be66f3981d82cae7e47
Ed <lookatemail@gmail.com>
2021-04-09 07:53:33 +0100

    Refactor oneTouch logo - load img via css and able to pass href to function onClick

--- dist/views/oneTouch/JS/authenticateUserNavBar.js
+++ dist/views/oneTouch/JS/authenticateUserNavBar.js
@@ -1,7 +1,19 @@
 import { _oneTouchUserAuthentication } from '../../../js/helperFunctions/mongoDB/oneTouchLogin/_oneTouchUserAuthentication.js';
 import { persistDOMData } from '../../../js/persistDOMData.js';
 
-_oneTouchUserAuthentication(); // User authentication
+// _oneTouchUserAuthentication(); // User authentication
 
 const endPoint = location.href.split('/').slice(-1)[0];
 persistDOMData(endPoint);
+
+document.querySelector('body').addEventListener('click', (event) => {
+  const href = event.target.getAttribute('href');
+
+  if (href) {
+    event.preventDefault();
+    console.log(event.target);
+    console.log(href);
+
+    return;
+  }
+});
